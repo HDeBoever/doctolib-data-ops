6752af4b1b16adad2e2b827bc564469ecc57b255
HDeBoever <henri.deboever@magics.tch>
2021-08-05 13:18:22 +0200

    Q3 created S3 bucket with folder/permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   profile = "default"
-  region  = "eu-west-3"
+  region  = var.aws_region
 }
 
 # Configure the EC2 instance details
@@ -46,4 +46,32 @@ resource "aws_key_pair" "kp-doctolib" {
   provisioner "local-exec" { 
     command = "echo '${tls_private_key.pk.private_key_pem}' > ./doctolib-private-key.pem"
   }
+}
+
+
+# Q3 : initialize an S3 bucket
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
+}
+
+# Create folder in the above S3 bucket
+resource "aws_s3_bucket_object" "can_be_written" {
+    bucket = "${aws_s3_bucket.my-s3-bucket.id}"
+    acl    = "private"
+    key    = "can_be_written/"
+    source = "/dev/null"
+}
+
+# Block public ACL access as per requirements 
+resource "aws_s3_bucket_public_access_block" "block_public_acl" {
+  bucket = "${aws_s3_bucket.my-s3-bucket.id}"
+  block_public_acls   = true
+  block_public_policy = true
 }
